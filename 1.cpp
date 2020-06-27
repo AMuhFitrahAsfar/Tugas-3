62870a27905360ebb3c74affcc7e770dc0564454
A Muh Fitrah Asfar 34 <fitrah4551@gmail.com>
2020-06-27 09:59:18 +0700

    Tugas 3

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int z=1;
+	
+	cout<<"Nilai z :"<<z<<endl;
+	cout<<"Nilai  dalam pointer dereference : "<<&z<<endl;	
+	
+	
+}
---
+++ 4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	int z=4;
+	float *a;
+	
+	a=&z;
+	cout<<"Hasil : "<<*a;	
+}
